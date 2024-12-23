1b2f018663ee636370a1a583f5e0007fffafcd3f
ujaas lohani <ujaaslohani2611@gmail.com>
2024-12-23 18:58:42 +0530

    backend separated

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,5 +1,4 @@
-  import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 import axios from 'axios';
 import './css/login.css';
 
